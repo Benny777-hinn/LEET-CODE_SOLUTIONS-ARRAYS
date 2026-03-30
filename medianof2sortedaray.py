330391524e7b0b81530ac90677eb970f17410841
Benny777-hinn <bhinn7375@gmail.com>
2026-03-30 19:44:30 +0530

    Add files via upload

---
+++ medianof2sortedaray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        merged = sorted(nums1 + nums2)
+        n = len(merged)
+
+        if n % 2 == 1:
+            return merged[n // 2]
+        else:
+            return (merged[n // 2 - 1] + merged[n // 2]) / 2.0
+        
+nums1 = [1, 3]
+nums2 = [2]
+print(Solution().findMedianSortedArrays(nums1, nums2))
