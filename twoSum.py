7a129c2549ffc7591d14cee238ea7ec8c1097c31
Benny777-hinn <bhinn7375@gmail.com>
2026-03-30 16:37:31 +0530

    Add files via upload

---
+++ twoSum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_map = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in num_map:
+                return [num_map[complement], i]
+
+            num_map[nums[i]] = i
+
+
+# Testing locally
+nums = [2, 7, 11, 15]
+target = 9
+
+print(Solution().twoSum(nums, target))
